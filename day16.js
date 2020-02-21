246f884fa5931e893d460915283979dad7f73379
yuhongpeng <yuhongpeng@lixiang.com>
2020-02-21 14:55:48 +0800

    FE 布局

---
+++ day16.js
@@ -0,0 +1,6 @@
+let scale = function() {
+    let tbox = document.getElementById('move-transition-box');
+    tbox.style.height = 200 + 'px';
+    tbox.style.width = 200 + 'px';
+    tbox.style.backgroundColor = 'red';
+}
