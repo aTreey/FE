0480c85998371488028b34b7bb5ac2b6829bf048
480814177@qq.com <480814177@qq.com>
2022-09-22 18:32:48 +0800

    feat: 使用eslint + prettier

--- React/src/App.js
+++ React/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <ul>
-        <li>React demo</li>
-        <li>React demo2</li>
-      </ul>
-    )
-  }
+	render() {
+		return (
+			<ul>
+				<li>React demo</li>
+				<li>React demo2</li>
+			</ul>
+		)
+	}
 }
 
 export default App
